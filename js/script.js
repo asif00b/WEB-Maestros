21a21a2e42f5e89030d3d67ebdae6e0ef89c7416
MD Asif <109533829+asif00b@users.noreply.github.com>
2022-11-25 03:59:19 +0600

    responsive banner

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
-var navMenu = document.getElementById("navMenu");
+var navMenu = document.getElementById("menu");
 function showMenu() {
-  navMenu.style.right = "20px";
+  menu.style.width = "200px";
+  menu.style.height = "400px";
+  bar.style.rotate = "-45deg";
 }
 function hideMenu() {
-  navMenu.style.right = "-300px";
+  menu.style.width = "0";
+  menu.style.height = "0";
+  bar.style.rotate = "360deg";
 }
